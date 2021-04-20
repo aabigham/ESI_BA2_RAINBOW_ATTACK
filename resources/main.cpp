7232c5d3cc927921197db9302346d0a22ccd77c9
Amine-Ayoub Bigham WSL <54985@etu.he2b.be>
2021-04-20 08:53:05 +0200

    static constexpr chars variable

--- resources/main.cpp
+++ resources/main.cpp
@@ -4,6 +4,11 @@
 
 #include "sha256.h"
 
+constexpr static char *chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    "abcdefghijklmnopqrstuvwxyz"
+    "0123456789";
+
 std::string reduce(std::string hash, unsigned index, unsigned passwdSize);
 
 int main(int argc, char const *argv[])
@@ -14,25 +19,22 @@ int main(int argc, char const *argv[])
         std::cerr << "File could not be opened\n";
         return -1;
     }
-    std::vector<std::pair<std::string, std::string>> data{};
-    std::string currPassword;
 
-    while (std::getline(fin, currPassword))
+    std::vector<std::pair<std::string, std::string>> datas{};
+    for (std::string currPassword; std::getline(fin, currPassword);)
     {
         std::string tail;
         for (size_t i = 0; i < 50000; i++)
         {
         }
     }
+
     return 0;
 }
 
 std::string reduce(std::string hash, unsigned index, unsigned passwdSize)
 {
     std::string reducedPwd;
-    const char *chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                        "abcdefghijklmnopqrstuvwxyz"
-                        "0123456789";
 
     for (unsigned i = 0; i < passwdSize; i++)
     {
