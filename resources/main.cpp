9fd89345e082b823146cefef477f2e191f8fd09b
54247 <54247@etu.he2b.be>
2021-04-23 19:03:20 +0200

    progress

--- resources/main.cpp
+++ resources/main.cpp
@@ -9,7 +9,8 @@
 #include "passwd-utils.hpp"
 
 std::string reduce(const std::string &hash, int index, int passwdSize = 8);
-void generateTable(std::ifstream &fin, std::ofstream &fout);
+void generateTable(std::ifstream &fin_pwd, std::ofstream &fout_table);
+void attack(std::ifstream &fin_hashes, std::ifstream &fin_rbtable, std::ofstream &fout_crackedPwd);
 
 int main(int argc, char const *argv[])
 {
@@ -24,77 +25,30 @@ int main(int argc, char const *argv[])
     //— passwords are stored after a single pass through the hash function,
 
     // Generate Rainbow Table
-    std::ofstream fout{"rb_table.txt"};
-    for (std::string currPassword; std::getline(fin_pwd, currPassword);)
-    {
-        auto pwdSize{currPassword.size()};
-        std::string tail = currPassword;
-        fout << currPassword << ',';
-        for (int i{0}; i < 50000; i++)
-        {
-            tail = sha256(tail);
-            tail = reduce(tail, i, pwdSize);
-        }
-        fout << tail << '\n';
-    }
+    std::ofstream fout_table{"rb_table.txt"};
+    generateTable(fin_pwd, fout_table);
     fin_pwd.close();
-    fout.close();
-
+    fout_table.close();
     // End generate
 
-    // Read hashes
+    // Attack
     std::ifstream fin_hashes{"hashes.txt"};
     std::ifstream fin_rbtable{"rb_table.txt"};
-    std::ofstream fout_crackedPwd{"cracked_pwd.txt"};
     if (fin_hashes.fail() || fin_rbtable.fail())
     {
         std::cerr << "Hash or rb table file could not be opened\n";
         return -1;
     }
-    bool found = false;
-    std::string currHash;
-    std::string rbLine;
-    while (std::getline(fin_hashes, currHash) && std::getline(fin_rbtable, rbLine))
-    {
-        //std::string token = line.substr(0, pos);
-        std::string head{strtok(&*rbLine.begin(), ",")};
-        std::string tail{strtok(NULL, ",")};
-        auto pwdSize = head.size();
-        std::string tempHash = currHash;
-        for (int i{0}; i < 50000 && !found; i++)
-        {
-            tempHash = reduce(tempHash, i, pwdSize);
-
-            //probleme de reduction
-            //probleme nb  de mdp
-            if (tempHash.compare(tail) == 0)
-            {
-                //finding the password
-                std::string currPassword = head;
-                std::string previousPassword;
-                for (int j{0}; j < 50000 && !found; j++)
-                {
-                    previousPassword = currPassword;
-                    currPassword = sha256(currPassword);
-                    if (currPassword.compare(currHash) == 0)
-                    {
-                        fout_crackedPwd << previousPassword << '\n';
-                        found = true;
-                    }
-                    currPassword = reduce(currPassword, j, pwdSize);
-                }
-            }
-            tempHash = sha256(tempHash);
-        }
-        found = false;
-    }
-
-    double success = rainbow::mass_check("cracked_pwd.txt", "hashes.txt");
-    std::cout << std::setprecision(4) << success << "% success" << std::endl;
-    // TODO
+    std::ofstream fout_crackedPwd{"cracked_pwd.txt"};
+    attack(fin_hashes, fin_rbtable, fout_crackedPwd);
     fin_hashes.close();
     fin_rbtable.close();
     fout_crackedPwd.close();
+    // End attack
+
+    double success = rainbow::mass_check("cracked_pwd.txt", "hashes.txt");
+    std::cout << std::setprecision(4) << success << "% success" << std::endl;
+
     return 0;
 }
 
@@ -118,6 +72,66 @@ std::string reduce(const std::string &hash, int index, int passwdSize)
         reduced += chars[current % 62];
     }
     return reduced;
+}
+
+void generateTable(std::ifstream &fin_pwd, std::ofstream &fout_table)
+{
+    for (std::string currPassword; std::getline(fin_pwd, currPassword);)
+    {
+        auto pwdSize{currPassword.size()};
+        std::string tail = currPassword;
+        fout_table << currPassword << ',';
+        for (int i{0}; i < 50000; i++)
+        {
+            tail = sha256(tail);
+            tail = reduce(tail, i, pwdSize);
+        }
+        fout_table << tail << '\n';
+    }
+}
+
+void attack(std::ifstream &fin_hashes, std::ifstream &fin_rbtable, std::ofstream &fout_crackedPwd)
+{
+    bool found = false;
+    std::string currHash;
+    std::string rbLine;
+    while (std::getline(fin_hashes, currHash) && std::getline(fin_rbtable, rbLine))
+    {
+        //std::string token = line.substr(0, pos);
+        std::string head{strtok(&*rbLine.begin(), ",")};
+        std::string tail{strtok(NULL, ",")};
+        auto pwdSize = head.size();
+        std::string tempHash = currHash;
+        for (int i{0}; i < 50000 && !found; i++)
+        {
+            tempHash = reduce(tempHash, i, pwdSize);
+
+            //std::cout << "1 Step " << i << " : " << tempHash << ", " << tail << std::endl;
+
+            //probleme de reduction
+            //probleme nb  de mdp
+            if (tempHash.compare(tail) == 0)
+            {
+                //finding the password
+                std::string currPassword = head;
+                std::string previousPassword;
+                for (int j{0}; j < 50000 && !found; j++)
+                {
+                    //     std::cout << "2 Step " << j << std::endl;
+                    previousPassword = currPassword;
+                    currPassword = sha256(currPassword);
+                    if (currPassword.compare(currHash) == 0)
+                    {
+                        fout_crackedPwd << previousPassword << '\n';
+                        found = true;
+                    }
+                    currPassword = reduce(currPassword, j, pwdSize);
+                }
+            }
+            tempHash = sha256(tempHash);
+        }
+        found = false;
+    }
 }
 
 /*int charToHexa(const char &c)
