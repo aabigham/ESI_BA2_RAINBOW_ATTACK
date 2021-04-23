14bc09dd1e6c7d078098e90d5aea611c0c9cf2a9
54247 <54247@etu.he2b.be>
2021-04-23 14:41:03 +0200

    Co-authored-by: aminebigham@hotmail.com <aminebigham@hotmail.com>

--- resources/main.cpp
+++ resources/main.cpp
@@ -1,64 +1,163 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
+#include <cstring>
 
 #include "sha256.h"
 
-std::string reduce(std::string hash, uint index, uint passwdSize);
+std::string reduce(const std::string &hash, int index, int passwdSize = 8);
+void generateTable(std::ifstream &fin, std::ofstream &fout);
 
 int main(int argc, char const *argv[])
 {
-    std::ifstream fin{argv[1]}; // Input file
-    if (fin.fail())
+    std::ifstream fin_pwd{argv[1]}; // Input file
+    if (fin_pwd.fail())
     {
-        std::cerr << "File could not be opened\n";
+        std::cerr << "Passwd file could not be opened\n";
         return -1;
     }
     //From a table of passwords stored as pairs "(login,hash)" with the help of some cryptographic
     //function H, you must implement a rainbow attack.
     //— passwords are stored after a single pass through the hash function,
 
-    std::ofstream fout{"rb_table.txt"}; // Rainbow Table
-    for (std::string currPassword; std::getline(fin, currPassword);)
+    // Generate Rainbow Table
+    std::ofstream fout{"rb_table.txt"};
+    for (std::string currPassword; std::getline(fin_pwd, currPassword);)
     {
+        auto pwdSize{currPassword.size()};
         std::string tail = currPassword;
         fout << currPassword << ',';
-        for (size_t i{0}; i < 50000; i++)
+        for (int i{0}; i < 50000; i++)
         {
             tail = sha256(tail);
-            tail = reduce(tail, i, currPassword.size());
+            tail = reduce(tail, i, pwdSize);
         }
         fout << tail << '\n';
     }
-    fin.close();
+    fin_pwd.close();
     fout.close();
+
+    // End generate
+
+    // Read hashes
+    std::ifstream fin_hashes{"hashes.txt"};
+    std::ifstream fin_rbtable{"rb_table.txt"};
+    std::ofstream fout_crackedPwd{"cracked_pwd.txt"};
+    if (fin_hashes.fail() || fin_rbtable.fail())
+    {
+        std::cerr << "Hash or rb table file could not be opened\n";
+        return -1;
+    }
+    bool found = false;
+    std::string matchingHash;
+    std::string currHash;
+    std::string rbLine;
+    while (std::getline(fin_hashes, currHash) && std::getline(fin_rbtable, rbLine))
+    {
+        std::string head{rbLine.substr(0, rbLine.find(","))};
+        std::string tail{rbLine.substr(1, rbLine.find(","))};
+        auto pwdSize = head.size();
+
+        std::string tempHash = currHash;
+        for (int i{0}; i < 50000 && !found; i++)
+        {
+            tempHash = reduce(tempHash, i, pwdSize);
+            //probleme de reduction
+            //probleme nb  de mdp
+            if (tempHash.compare(tail) == 0)
+            {
+                std::cout << "MATCH PWD" << std::endl;
+
+                //finding the password
+                std::string currPassword = head;
+                std::string previousPassword;
+                for (int j{0}; j < 50000 && !found; j++)
+                {
+                    previousPassword = currPassword;
+                    currPassword = sha256(currPassword);
+                    if (currPassword.compare(currHash) == 0)
+                    {
+                        std::cout << "MATCH HASH" << std::endl;
+
+                        fout_crackedPwd << previousPassword << '\n';
+                        found = true;
+                    }
+                    currPassword = reduce(currPassword, j, pwdSize);
+                }
+            }
+            tempHash = sha256(tempHash);
+        }
+        found = false;
+    }
+
+    // TODO
+    fin_hashes.close();
+    fin_rbtable.close();
+    fout_crackedPwd.close();
     return 0;
 }
 
-std::string string_to_hex(const std::string &input)
+std::string reduce(const std::string &hash, int index, int passwdSize)
 {
-    static const char hex_digits[] = "0123456789ABCDEF";
-    std::string output;
-    output.reserve(input.length() * 2);
-    for (unsigned char c : input)
+    static const char chars[]{"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"};
+    //
+    unsigned char bytes[16];
+    int temp;
+    for (int i = 0; i < 16; ++i)
     {
-        output.push_back(hex_digits[c >> 4]);
-        output.push_back(hex_digits[c & 15]);
+        std::sscanf(hash.c_str() + 2 * i, "%02x", &temp);
+        bytes[i] = temp;
     }
-    return output;
+    //
+    std::string reduced;
+    int current;
+    for (int j = 0; j < passwdSize; ++j)
+    {
+        current = bytes[(j + index) % 16];
+        reduced += chars[current % 62];
+    }
+    return reduced;
 }
 
-std::string reduce(std::string hash, uint index, uint passwdSize)
+/*int charToHexa(const char &c)
+{
+    
+    switch (c)
+    {
+    case 'a':
+        return 10;
+    case 'b':
+        return 11;
+    case 'c':
+        return 12;
+    case 'd':
+        return 13;
+    case 'e':
+        return 14;
+    case 'f':
+        return 15;
+
+    default:
+        exit(1);
+    }
+}*/
+
+/*std::string reduce(std::string hash, int index, int passwdSize)
 {
     static const char chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     std::string reducedPwd;
-    std::string bytes{string_to_hex(hash)};
-    //int n = (int)strtol(hash.c_str(), NULL, 16);
+
     int n;
-    for (uint i{0}; i < passwdSize; i++)
+    for (int i{0}; i < passwdSize; i++)
     {
-        n = bytes[(n + index) % 16];
+        const char *curr{&hash.at(i)};
+        if (isdigit(*curr))
+            n = (((int)strtol(curr, NULL, 10) + index) % (int)std::pow(62, passwdSize));
+        else
+            n = ((charToHexa(*curr) + index) % (int)std::pow(62, passwdSize));
+
         reducedPwd += chars[n % 62];
     }
     return reducedPwd;
-}
+}*/
